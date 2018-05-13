9ac5dce64bacb65f1a4a9903ee8cef96a302db0f
Evan Klitzke <evan@eklitzke.org>
2018-05-12 22:34:02 -0700

    Fix null-termination issue with Wayland clipboard buffers

--- widget/gtk/nsClipboardWayland.cpp
+++ widget/gtk/nsClipboardWayland.cpp
@@ -671,9 +671,11 @@ nsRetrievalContextWayland::TransferFastTrackClipboard(
         if (dataLength > 0) {
             mClipboardDataLength = dataLength;
             mClipboardData = reinterpret_cast<char*>(
-                g_malloc(sizeof(char)*mClipboardDataLength));
-            memcpy(mClipboardData, gtk_selection_data_get_data(aSelectionData),
-                   sizeof(char)*mClipboardDataLength);
+                g_malloc(sizeof(char)*(mClipboardDataLength+1)));
+            memcpy(mClipboardData,
+                   (const char *)gtk_selection_data_get_data(aSelectionData),
+                   sizeof(char)*(mClipboardDataLength+1));
+            mClipboardData[mClipboardDataLength] = '\0';
         }
     } else {
         NS_WARNING("Received obsoleted clipboard data!");
